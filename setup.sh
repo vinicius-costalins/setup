84259238bffa9e281df5f16175b4d9bf256a81e9
Daniel Lopes <danielorkae@outlook.com>
2021-03-30 20:37:13 -0300

    install dev tools

--- setup.sh
+++ setup.sh
@@ -23,3 +23,19 @@ curl https://raw.githubusercontent.com/danielorkae/setup/main/.zshrc > $HOME/.zs
 
 # Initialize zsh
 zsh
+
+# Install NodeJS LTS
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Install Yarn
+sudo npm --global install yarn
+
+# Install PHP
+sudo apt install -y php
+
+# Install Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --install-dir=/usr/bin --filename=composer
+php -r "unlink('composer-setup.php');"
